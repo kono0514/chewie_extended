960ff532af3fe955ca3bb4050aa372a053b962c7
Uuganbaatar <38470934+kono0514@users.noreply.github.com>
2020-11-09 19:28:50 +0800

    Allow speed changing

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -250,6 +250,7 @@ class ChewieController extends ChangeNotifier {
     this.isLive = false,
     this.allowFullScreen = true,
     this.allowMuting = true,
+    this.allowSpeedChanging = true,
     this.systemOverlaysAfterFullScreen = SystemUiOverlay.values,
     this.deviceOrientationsAfterFullScreen = const [
       DeviceOrientation.portraitUp,
@@ -327,6 +328,9 @@ class ChewieController extends ChangeNotifier {
 
   /// Defines if the mute control should be shown
   final bool allowMuting;
+
+  /// Defines if the playback speed control should be shown
+  final bool allowSpeedChanging;
 
   /// Defines the system overlays visible after exiting fullscreen
   final List<SystemUiOverlay> systemOverlaysAfterFullScreen;
